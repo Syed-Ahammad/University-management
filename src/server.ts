6330ffbf546f87fe15817f82b9f511d33f24585e
Syed-Ahammad <105214007+Syed-Ahammad@users.noreply.github.com>
2023-06-03 17:24:16 +0400

    project setup

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/index";
+
+
+
+async function bootstrap() {
+    
+
+
+    try {
+        app.listen(config.port, () => {
+            console.log(`application listening on port ${config.port}`);
+          })
+        await mongoose.connect(config.DatabaseUrl as string);
+        console.log('database connection successful');
+    }
+    catch(err) {
+
+        console.log("failed to connect database",err);
+    }
+  };
+
+  bootstrap()
