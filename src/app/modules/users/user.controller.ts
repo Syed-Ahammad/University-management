7b2b7cb0dd5d0216c534123efe04d236de0f8b3c
Syed-Ahammad <105214007+Syed-Ahammad@users.noreply.github.com>
2023-10-01 19:43:25 +0400

    git config system local global push.autosetupremote true

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,28 +1,26 @@
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import sendResponse from '../../../shared/sendResponse';
 
-const createUser = catchAsync(
-  async (req: Request, res: Response) => {
-    const { user } = req.body;
-    const result = await UserService.createUser(user);
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const { user } = req.body;
+  const result = await UserService.createUser(user);
 
-    // res.status(200).json({
-    //   success: true,
-    //   message: 'User created successfully',
-    //   data: result,
-    // });
+  // res.status(200).json({
+  //   success: true,
+  //   message: 'User created successfully',
+  //   data: result,
+  // });
 
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'User created successfully',
-      data: result,
-    });
-  }
-);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User created successfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createUser,
